a453a0d545c8da185fd9ca8fdaf4edd85a121024
MasashiHashiguchi <89846357+MasashiHashiguchi@users.noreply.github.com>
2023-11-13 10:48:55 +0000

    Change some parts Hello.py

--- Hello.py
+++ Hello.py
@@ -1,16 +1,4 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 
 import streamlit as st
 from streamlit.logger import get_logger
@@ -20,29 +8,17 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="Dev Support",
+        page_icon="🛠️",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
+    st.write("# Welcome to Dev Support 👋")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        Dev SupportはtoC向けに開発されたアプリケーションです。
+        社内エンジニアの検索機能と携わるプロジェクトを入力してスキルの類似度が高いエンジニアをレコメンドする機能を備えています。
+        サイドバーからあなたの目的にあった機能を試してみましょう！
     """
     )
 
