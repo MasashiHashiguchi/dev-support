3e760b698ab6639ceb45d490f4f3a4356e293e58
MasashiHashiguchi <89846357+MasashiHashiguchi@users.noreply.github.com>
2023-11-13 11:48:59 +0000

    Delete some codes

--- pages/LLM_recommendation_demo.py
+++ pages/LLM_recommendation_demo.py
@@ -35,34 +35,33 @@ def sequential_chain(user_input):
     return overall_chain.run(user_input)
 
 
-with suggest_with_input_dataset_tab:
-    st.header('Suggest Inhouse Engineer with Input Dataset')
-    st.write('👈まず初めに、データセットをアップロードしてください！')
-    # Load CSV file and process the data
-    if uploaded_file:
-        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-            tmp_file.write(uploaded_file.getvalue())
-            tmp_file_path = tmp_file.name
+st.header('Suggest Inhouse Engineer with Input Dataset')
+st.write('👈まず初めに、データセットをアップロードしてください！')
+# Load CSV file and process the data
+if uploaded_file:
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(uploaded_file.getvalue())
+        tmp_file_path = tmp_file.name
 
-        raw_csv = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={'delimiter': ','})
+    raw_csv = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={'delimiter': ','})
 
-        dataset = raw_csv.load()
+    dataset = raw_csv.load()
 
-        text_splitter_dataset = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    text_splitter_dataset = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 
-        documents = text_splitter_dataset.split_documents(dataset)
+    documents = text_splitter_dataset.split_documents(dataset)
 
-        database = Chroma.from_documents(documents, OpenAIEmbeddings())
+    database = Chroma.from_documents(documents, OpenAIEmbeddings())
 
-        with st.form(key='my_form', clear_on_submit=True):
+    with st.form(key='my_form', clear_on_submit=True):
 
-            user_input = st.text_input("Query:", placeholder="Write a project title or summary here", key='input')
-            submit_button = st.form_submit_button(label='Send')
+        user_input = st.text_input("Query:", placeholder="Write a project title or summary here", key='input')
+        submit_button = st.form_submit_button(label='Send')
 
-            if submit_button and user_input:
-                response = sequential_chain(user_input)
-                output = database.similarity_search(response)
-                st.write("Here's necessary skills suggested from the project based on your input👇")
-                st.write(response)
-                st.write("Here's an inhouse engineer recommended based on the input dataset👇")
-                st.write(output[0].page_content)
+        if submit_button and user_input:
+            response = sequential_chain(user_input)
+            output = database.similarity_search(response)
+            st.write("Here's necessary skills suggested from the project based on your input👇")
+            st.write(response)
+            st.write("Here's an inhouse engineer recommended based on the input dataset👇")
+            st.write(output[0].page_content)
