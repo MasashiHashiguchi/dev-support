27f1f651108f9bbbc74c58c2478470bc68a1e4b5
MasashiHashiguchi <89846357+MasashiHashiguchi@users.noreply.github.com>
2023-11-14 01:20:16 +0000

    Get back to original

--- Hello.py
+++ Hello.py
@@ -3,6 +3,25 @@
 import streamlit as st
 from streamlit.logger import get_logger
 
-st.header("Welcome to Dev Support 🤚")
-st.write("Dev SupportはtoC向けに開発されたアプリケーションです。社内エンジニアの検索機能と携わるプロジェクトを入力してスキルの類似度が高いエンジニアをレコメンドする機能を備えています。サイドバーからあなたの目的にあった機能を試してみましょう!")
+LOGGER = get_logger(__name__)
 
+
+def run():
+    st.set_page_config(
+        page_title="Dev Support",
+        page_icon="🛠️",
+    )
+
+    st.write("# Welcome to Dev Support 👋")
+
+    st.markdown(
+        """
+        Dev SupportはtoC向けに開発されたアプリケーションです。
+        社内エンジニアの検索機能と携わるプロジェクトを入力してスキルの類似度が高いエンジニアをレコメンドする機能を備えています。
+        サイドバーからあなたの目的にあった機能を試してみましょう！
+    """
+    )
+
+
+if __name__ == "__main__":
+    run()
