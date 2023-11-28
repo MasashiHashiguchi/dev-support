f2d5d22368d0ab33ae662a369078e67114188411
MasashiHashiguchi <89846357+MasashiHashiguchi@users.noreply.github.com>
2023-11-28 04:30:57 +0000

    Delete placeholder

--- pages/LLM_recommendation_demo.py
+++ pages/LLM_recommendation_demo.py
@@ -145,20 +145,12 @@ if uploaded_file:
         "### Describe your project"
 
         #user_input = st.text_input('プロジェクト名の入力:')
-        name = st.text_input('Input project name:', placeholder='Personalized Content Recommendation Engine for Online Streaming Platform')
-        overview = st.text_area('Input project overview', placeholder="""In this project description, we present the details of an IT-company project focused on leveraging Machine Learning (ML) and Data Science to develop an innovative recommendation engine for an online streaming platform. The project aims to enhance user experience, increase engagement, and optimize content recommendations based on individual preferences.
-        """)
+        name = st.text_input('Input project name:')
+        overview = st.text_area('Input project overview')
         complete_by = st.date_input('Choose project completion date')
-        goals = st.text_area('Input project goals', placeholder="""The primary objective of this IT-company project is to design, build, and deploy a sophisticated recommendation engine that utilizes Machine Learning and Data Science techniques to:
-        1. Enhance User Engagement: Provide users with tailored content recommendations that match their viewing history, preferences, and behaviors.
-        2. Improve Content Discovery: Facilitate the discovery of new content by suggesting relevant movies, TV shows, and genres that align with users' interests.
-        3. Optimize Viewing Experience: Increase user satisfaction by reducing the time spent searching for content and improving the relevance of recommendations.
-        4. Boost Platform Retention: Encourage users to spend more time on the platform by consistently delivering compelling and personalized content.
-        5. Drive Business Value: Translate improved user engagement into higher viewer retention rates, increased subscriptions, and enhanced brand loyalty.
-        """)
+        goals = st.text_area('Input project goals')
         positions = st.multiselect('Choose required positions', position_retrieve(tmp_file_path))
-        desired_outcomes = st.text_area('Input project desired outcomes', placeholder="""The successful completion of this project will result in a cutting-edge recommendation engine integrated into the online streaming platform. The engine will deliver accurate and personalized content suggestions to users, ultimately enhancing their viewing experience, increasing engagement, and contributing to the platform's business success.
-        """)
+        desired_outcomes = st.text_area('Input project desired outcomes')
         match_button = st.form_submit_button(label="Submit")
 
         duration = str(complete_by - datetime.date.today())
